6cc5c34f3cdede01221e5680b4c86b4c9c127399
Andrew Han <andrewthehan@gmail.com>
2020-02-02 00:15:18 -0800

    Bind search to ctrl+f.

--- src/components/FrameHeader.js
+++ src/components/FrameHeader.js
@@ -5,9 +5,9 @@ import {
   Toolbar,
   Typography
 } from "@material-ui/core";
-import React from "react";
-import { isAnyNull } from "../utils/ObjectUtils";
+import React, { useEffect, useRef } from "react";
 import { debounce } from "../utils/FunctionUtils";
+import { isAnyNull } from "../utils/ObjectUtils";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -27,6 +27,24 @@ const useStyles = makeStyles(theme => ({
 export function FrameHeader({ children, title, onSearch = null }) {
   const classes = useStyles();
 
+  const searchInputRef = useRef(null);
+
+  useEffect(() => {
+    const giveSearchFocus = e => {
+      if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
+        e.preventDefault();
+
+        searchInputRef.current.focus();
+      }
+    };
+
+    window.addEventListener("keydown", giveSearchFocus);
+
+    return () => {
+      window.removeEventListener("keydown", giveSearchFocus);
+    };
+  }, [searchInputRef]);
+
   const onSearchDebounced = debounce(onSearch, 250);
   const handleChange = e => {
     onSearchDebounced(e.target.value);
@@ -41,7 +59,7 @@ export function FrameHeader({ children, title, onSearch = null }) {
       <InputBase
         className={classes.input}
         placeholder="Search"
-        inputProps={{ "aria-label": "search" }}
+        inputProps={{ "aria-label": "search", ref: searchInputRef }}
         onChange={handleChange}
       />
     );
